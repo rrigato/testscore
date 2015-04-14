fcf3a6c047726f0ab4fd8e4230d1dd9807e0c4c6
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-04-14 14:43:21 -0700

    Create testscores.cpp

---
+++ testscores.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+class TestScores
+{
+private:
+	int * intptr = 0;
+	int array_size = 0;
+	intptr = new int[array_size];
+public:
+	TestScores(int, int, int)
+};
+TestScores::TestScores(int test1, int test2, int test3)
+{
+	intptr
+}
