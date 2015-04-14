53aafd1fefe3a58a59a9d83e365074ace524c6a7
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-04-14 15:27:46 -0700

    Update testscores.cpp

--- testscores.cpp
+++ testscores.cpp
@@ -8,22 +8,49 @@ private:
 
 
 public:
+	TestScores();
 	TestScores(int);
-	~TestScores();
+	~TestScores(); // dealocates dynamically allocated memory
+	void set_tests(int, int); // mutator
+	int get_number_of_tests() const;
 };
+
+TestScores::TestScores()
+{
+	intptr = new int[5];
+}
 TestScores::TestScores(int size)
 {
 	intptr = new int [size];
+}
+
+void TestScores::set_tests(int element_number, int test_score)
+{
+	intptr[element_number] = test_score;
 }
 
 TestScores::~TestScores()
 {
 	delete[] intptr;
 }
+TestScores::get_number_of_tests() const
+{
+
+}
 int main()
 {
 	int  array_size = 0;
 	std::cout << "Enter how many test scores you have " << std::endl;
 	std::cin >> array_size;
+	TestScores tests(array_size);
+	for (int i = 0; i < array_size; i++)
+	{
+		int score = 0;
+		std::cout << "Enter the test score number " << i + 1 << std::endl;
+		std::cin >> score;
+		tests.set_tests(i, score);
+	}
+
+
 	return 0;
 }
